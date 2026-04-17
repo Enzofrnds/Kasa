b0136c06509e05d22f9289a5767c4ca1482c01fd
Enzofrnds <enzofernandes3@outlook.fr>
2026-04-17 03:51:46 +0200

    Ajout du fichier annonces.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
-import Home from './Home'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
