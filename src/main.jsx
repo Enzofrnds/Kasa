1f116602b32cb3cf0f5b9525bff9e441a25f811b
Enzofrnds <enzofernandes3@outlook.fr>
2026-04-17 07:33:25 +0200

    feat : ajoute de la page d'erreur

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import Header from './components/Header';
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import Error from './pages/Error';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')).render(
       <Header />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='*' element={<Error />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Error/index.jsx
@@ -0,0 +1,15 @@
+import './Error.css'
+import { Link } from 'react-router-dom'     
+
+function Error() {
+  return (
+    <main className="error">
+        <h1 className="error__title">404</h1>
+        <p className="error__text">Oups! La page que vous demandez n'existe pas.</p>
+        <Link className="error__link" to="/">
+            Retourner sur la page d'accueil
+        </Link>
+    </main>
+  );
+}
+export default Error
