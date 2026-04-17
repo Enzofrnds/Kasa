3a6e15a6c1a487ea5a8dcb7031ebf88e9823ebfe
Enzofrnds <enzofernandes3@outlook.fr>
2026-04-17 02:49:15 +0200

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Home from './Home'
+import './index.css'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Router>
+      <Routes>
+      </Routes>
+    </Router>
+  </StrictMode>,
+)
