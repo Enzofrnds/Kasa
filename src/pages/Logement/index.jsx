74c9d5598d3472d50dc32d1208decebd3ca5682c
Enzofrnds <enzofernandes3@outlook.fr>
2026-04-20 22:23:21 +0200

    feat: redirection vers la page 404 lors si l'annonce n'existe pas

--- src/pages/Logement/index.jsx
+++ src/pages/Logement/index.jsx
@@ -3,26 +3,30 @@ import Annonce from '../../datas/annonces.json';
 import arrowLeft from '../../assets/arrow-left.png';
 import arrowRight from '../../assets/arrow-right.png';
 import Accordion from '../../components/Accordion';
-import { useParams } from 'react-router-dom';
+import Error from '../Error';
+import { useNavigate , useParams } from 'react-router-dom';
+import { use } from 'react';
 
 function Logement() {
     const { id } = useParams();
     const annonce = Annonce.find((item) => item.id === id);
     const rating = Math.max(0, Math.min(5, Number(annonce?.rating) || 0));
 
-    return (
+    return !annonce ? (
+        <Error />
+    ) : (
         <main>
             <div className='carrousel'>
                 <img className=' arrow arrow-left' src={arrowLeft} alt="Précédent"/>
-                <img className='carrousel-img' src={annonce?.cover} alt={annonce?.title} />
+                <img className='carrousel-img' src={annonce.cover} alt={annonce.title} />
                 <img className='arrow arrow-right' src={arrowRight} alt="Suivant"/>
             </div>
             <section className='info'>
                 <div className='info-left'>
-                    <h1 className='info-title'>{annonce?.title}</h1>
-                    <p className='info-location'>{annonce?.location}</p>
+                    <h1 className='info-title'>{annonce.title}</h1>
+                    <p className='info-location'>{annonce.location}</p>
                     <div className='tags'>
-                        {annonce?.tags.map((tag, index) => (
+                        {annonce.tags.map((tag, index) => (
                             <span key={index} className='tag'>{tag}</span>
                         ))}
                     </div>
@@ -30,22 +34,21 @@ function Logement() {
                 <div className='info-right'>
                     <div className='host'>
                         <p className='host-name'>{annonce?.host.name}</p>
-                        <img className='host-picture' src={annonce?.host.picture} alt={annonce?.host.name} />
+                        <img className='host-picture' src={annonce.host.picture} alt={annonce.host.name} />
                     </div>
                     <div className='rating'>
                         {Array.from({ length: 5 }, (_, index) => (
                             <i
                                 key={index}
                                 className={`star ${index < rating ? 'fa-solid active' : 'fa-solid inactive '} fa-star`}
-                                aria-hidden='true'
                             />
                         ))}
                     </div>
                 </div>
                 <div className='accordions'>
-                        <Accordion title="Description" content={annonce?.description} />
-                        <Accordion title="Équipements" content={annonce?.equipments.map((equipment, index) => (
-                            <span key={index}>{equipment}</span>
+                        <Accordion title="Description" content={annonce.description} />
+                        <Accordion title="Équipements" content={annonce.equipments.map((equipment, index) => (
+                            <p key={index}>{equipment}</p>
                         ))} />
                     </div>
             </section>
